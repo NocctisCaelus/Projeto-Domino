4b49d682776d1ade16290f22e054d034045ce597
NocctisCaelus <69806631+NocctisCaelus@users.noreply.github.com>
2020-09-18 08:20:55 -0300

    Add files via upload

---
+++ App.c
@@ -0,0 +1,14 @@
+//Projeto Domino
+//Laboratorio de Programação
+//Grupo Racionais
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include "controller.h"
+
+int main() {
+
+    rodaJogo();
+
+}
